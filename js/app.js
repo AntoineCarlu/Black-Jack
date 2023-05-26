2ea817f79b54e01e354340de49dae30e3d84e2a8
6Yoru6 <carluantoine66@outlook.com>
2023-05-26 13:15:30 +0200

    draw

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 //define the 4 familly of the 52 cards cardgame
-let cgCards = ['1h','1d','1c','1s','2h','2d','2c','2s','3h','3d','3c','3s','4h','4d','4c','4s','5h','5d','5c','5s','6h','6d','6c','6s','7h','7d','7c','7s','8h','8d','8c','8s','9h','9d','9c','9s','10h','10d','10c','10s','Vh','Vd','Vc','Vs','Qh','Qd','Qc','Qs','Kh','Kd','Kc','Ks'];
+var cgCards = ['1h','1d','1c','1s','2h','2d','2c','2s','3h','3d','3c','3s','4h','4d','4c','4s','5h','5d','5c','5s','6h','6d','6c','6s','7h','7d','7c','7s','8h','8d','8c','8s','9h','9d','9c','9s','10h','10d','10c','10s','Vh','Vd','Vc','Vs','Qh','Qd','Qc','Qs','Kh','Kd','Kc','Ks'];
 //define two empty table for cards of the player and the GM
 var PlayerCards = [];
 var GmCards = [];
+//
+
 
 //Function to get a random card
 function RandomCard() {
@@ -12,9 +14,11 @@ function RandomCard() {
 //Event to launch game when player click on the button start
 document.getElementById("play").addEventListener('click', PlayGame);
 function PlayGame() {
-  //get the two cards table empty for new game
-  let PlayerCards = [];
-  let GmCards = [];
+    console.clear();
+  //clear the cards and hands when start new game
+  PlayerCards = [];
+  GmCards = [];
+  cgCards = ['1h','1d','1c','1s','2h','2d','2c','2s','3h','3d','3c','3s','4h','4d','4c','4s','5h','5d','5c','5s','6h','6d','6c','6s','7h','7d','7c','7s','8h','8d','8c','8s','9h','9d','9c','9s','10h','10d','10c','10s','Vh','Vd','Vc','Vs','Qh','Qd','Qc','Qs','Kh','Kd','Kc','Ks'];
 
   //Give two random cards to the player and GM
   for (var i = 0; i<2; i++) {
@@ -27,9 +31,17 @@ function PlayGame() {
     cgCards.splice(randomCard, 1); //same as the top
     GmCards.push(GmCard); //same as the top
   }
-
   //console infos
   console.log('Cartes du joueur : ',PlayerCards);
   console.log('Cartes du GM :',GmCards);
 }
 
+//Event to draw cards to add them to the hand of the player
+document.getElementById("draw").addEventListener('click', DrawCard);
+function DrawCard() {
+  var randomCard = RandomCard();
+  var PlayerCard = cgCards[randomCard];
+  cgCards.splice(randomCard, 1); //remove the card taken in the table cgCards
+  PlayerCards.push(PlayerCard); //push the random card obtained in the table of the player
+    console.log('Cartes du joueur : ',PlayerCards);
+}
